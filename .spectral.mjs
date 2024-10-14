cbf6f74a92c23b539f8422d224fd09b78dabcf9e
Nauman <mnaumanali94@gmail.com>
2024-10-14 15:50:13 +0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1Nzc3NQ/spectral.js?branch_slug=master&token=b40ddb45-4d10-4884-9b5c-50f36a9aebdd";
+export default { extends: ruleset };
